a9471db9f33be30218a513ad18eb3403a2cd2dd8
yogeshrdr <yashrdr11@gmail.com>
2023-03-20 10:33:42 +0530

    useNavigate navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../../atom';
 import './navbar.css';
 import navbarRoutes from './navbar.routes';
 
 
 function Navbar() {
+  const navigate = useNavigate();
 
+  const onNavClick = (link) => {
+    navigate(link);
+  }
   return (
     <div className='navbar'>
         {navbarRoutes.map((nav) => (
-          <Button key={nav.name} type="navbarbtn">{nav.name}</Button> 
+          <Button key={nav.name} type="navbarbtn" onClick={() => navigate(nav.link)}>{nav.name}</Button> 
         ))}
     </div>
   )
