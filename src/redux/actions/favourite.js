148de3f0c4000036569b8deef9bd9544363f55ce
yogeshrdr <yashrdr11@gmail.com>
2023-03-24 12:09:08 +0530

    Redux Follow Resolved

--- src/redux/actions/favourite.js
+++ src/redux/actions/favourite.js
@@ -1,4 +1,4 @@
 import { SET_FAVOURITE_PEOPLE, SET_FAVOURITE_SHOW } from "../constant";
 
-export const FAVOURITE_SHOW = (id) => ({type: SET_FAVOURITE_SHOW, payload: {id}});
-export const FAVOURITE_PEOPLE = (id) => ({type: SET_FAVOURITE_PEOPLE, payload: {id}});
+export const FAVOURITE_SHOW = (data) => ({type: SET_FAVOURITE_SHOW, payload: {data}});
+export const FAVOURITE_PEOPLE = (data) => ({type: SET_FAVOURITE_PEOPLE, payload: {data}});
